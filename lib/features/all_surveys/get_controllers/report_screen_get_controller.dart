9a0dd004015c08949eb20ff68e6c6e70f70da9c0
= <44868912+jonathanjohn47@users.noreply.github.com>
2022-09-19 10:17:10 +0530

    printing and sharing PDF.

--- lib/features/all_surveys/get_controllers/report_screen_get_controller.dart
+++ lib/features/all_surveys/get_controllers/report_screen_get_controller.dart
@@ -2,6 +2,7 @@ import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:ikkjutt_jammu_survey_app/features/all_surveys/models/report_model.dart';
 import 'package:ikkjutt_jammu_survey_app/features/new_survey/models/survey_model.dart';
+import 'package:ikkjutt_jammu_survey_app/helpers/date_time_helper.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:printing/printing.dart';
@@ -31,24 +32,143 @@ class ReportScreenGetController extends GetxController {
       (await rootBundle.load('assets/images/logo.jpeg')).buffer.asUint8List(),
     );
     PdfGoogleFonts.nunitoRegular().then((font) {
-      pdf.addPage(pw.Page(
+      pdf.addPage(pw.MultiPage(
+          maxPages: 100,
           pageFormat: PdfPageFormat.a4,
+          orientation: pw.PageOrientation.landscape,
           build: (pw.Context context) {
-            return pw.Column(children: [
-              pw.Row(children: [
-                pw.Image(
-                  image,
-                ),
-                pw.Text(
-                  'SURVEY REPORT',
-                  style: pw.TextStyle(
-                    fontSize: 20,
-                    fontWeight: pw.FontWeight.bold,
-                    font: font,
-                  ),
-                ),
-              ]),
-            ]);
+            return [
+              pw.Row(
+                  mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+                  children: [
+                    pw.SizedBox(
+                        width: PdfPageFormat.a4.width * 0.2,
+                        height: PdfPageFormat.a4.width * 0.2,
+                        child: pw.Image(
+                          image,
+                        )),
+                    pw.Text(
+                      'SURVEY REPORT',
+                      style: pw.TextStyle(
+                        fontSize: 40,
+                        fontWeight: pw.FontWeight.bold,
+                        font: font,
+                      ),
+                    ),
+                  ]),
+              pw.SizedBox(height: 20),
+              pw.Row(
+                  mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+                  children: [
+                    pw.Text(
+                      'Survey Title:',
+                      style: pw.TextStyle(
+                        fontSize: 20,
+                        font: font,
+                      ),
+                    ),
+                    pw.Text(
+                      allReports[0].surveyModel.title,
+                      style: pw.TextStyle(
+                        fontSize: 25,
+                        fontWeight: pw.FontWeight.bold,
+                        font: font,
+                      ),
+                    ),
+                  ]),
+              pw.Row(
+                  mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+                  children: [
+                    pw.Text(
+                      'Date:',
+                      style: pw.TextStyle(
+                        fontSize: 20,
+                        font: font,
+                      ),
+                    ),
+                    pw.Text(
+                      surveyModel.scheduledAt.dateStringWithMonthName,
+                      style: pw.TextStyle(
+                        fontSize: 25,
+                        fontWeight: pw.FontWeight.bold,
+                        font: font,
+                      ),
+                    ),
+                  ]),
+              pw.Row(
+                  mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+                  children: [
+                    pw.Text(
+                      'Area:',
+                      style: pw.TextStyle(
+                        fontSize: 20,
+                        font: font,
+                      ),
+                    ),
+                    pw.Text(
+                      surveyModel.area,
+                      style: pw.TextStyle(
+                        fontSize: 25,
+                        fontWeight: pw.FontWeight.bold,
+                        font: font,
+                      ),
+                    ),
+                  ]),
+              pw.Table(
+                  columnWidths: {
+                    0: pw.FlexColumnWidth(1),
+                    ...Map.fromIterable(
+                      allReports,
+                      key: (e) => allReports.indexOf(e) + 1,
+                      value: (e) => pw.FlexColumnWidth(5),
+                    ),
+                  },
+                  border: pw.TableBorder.all(),
+                  children: [
+                    pw.TableRow(children: [
+                      pw.Container(),
+                      ...allReports[0]
+                          .surveyModel
+                          .questions
+                          .map((e) => pw.Padding(
+                          padding: pw.EdgeInsets.all(4),
+                          child: pw.Text(
+                            e.questionText,
+                            style: pw.TextStyle(
+                              fontSize: 20,
+                              fontWeight: pw.FontWeight.bold,
+                              font: font,
+                            ),
+                          )))
+                          .toList(),
+                    ]),
+                    ...allReports
+                        .map((e) => pw.TableRow(children: [
+                      pw.Padding(
+                          padding: pw.EdgeInsets.all(4),
+                          child: pw.Text(
+                            (allReports.indexOf(e) + 1).toString(),
+                            style: pw.TextStyle(
+                              fontSize: 20,
+                              fontWeight: pw.FontWeight.bold,
+                              font: font,
+                            ),
+                          )),
+                      ...e.surveyModel.questions
+                          .map((e) => pw.Padding(
+                          padding: pw.EdgeInsets.all(4),
+                          child: pw.Text(
+                            e.answerText,
+                            style: pw.TextStyle(
+                              fontSize: 20,
+                              font: font,
+                            ),
+                          )))
+                          .toList(),
+                    ]))
+                        .toList(),
+                  ]),
+            ];
           }));
       pdf.save().then((value) {
         SystemChrome.setPreferredOrientations([
--- lib/helpers/date_time_helper.dart
+++ lib/helpers/date_time_helper.dart
@@ -3,6 +3,25 @@ extension DateTimeHelper on DateTime {
     return '${this.day}/${this.month}/${this.year}';
   }
 
+  String get dateStringWithMonthName {
+    Map<int, String> monthMap = {
+      1: 'January',
+      2: 'February',
+      3: 'March',
+      4: 'April',
+      5: 'May',
+      6: 'June',
+      7: 'July',
+      8: 'August',
+      9: 'September',
+      10: 'October',
+      11: 'November',
+      12: 'December',
+    };
+
+    return '${this.day.toString().padLeft(2, '0')} ${monthMap[this.month]} ${this.year}';
+  }
+
   String get dateTimeString {
     return '${this.day.toString().padLeft(2, '0')}/${this.month.toString().padLeft(2, '0')}/${this.year} ${this.hour.toString().padLeft(2, '0')}:${this.minute.toString().padLeft(2, '0')}';
   }
